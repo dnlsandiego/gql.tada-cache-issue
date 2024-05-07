2dc713c645e11f0f2d28c10bc8b4218bd9d4793a
Daniel Sandiego <8008023+dnlsandiego@users.noreply.github.com>
2024-05-07 03:48:54 +0000

    cleanup

---
+++ src/pokemon.tsx
@@ -0,0 +1,51 @@
+import { useQuery } from 'urql';
+import { graphql } from '../graphql';
+
+import { PokemonItem, PokemonItemFragment } from './pokemon-item';
+
+const PokemonsQuery = graphql(`
+  query Pokemons ($limit: Int = 10) {
+    pokemons(limit: $limit) {
+      id
+      ...PokemonItem
+    }
+  }
+`, [PokemonItemFragment]);
+
+const PersistedQuery = graphql.persisted(
+  "sha256:fc073da8e9719deb51cdb258d7c35865708852c5ce9031a257588370d3cd42f3",
+  PokemonsQuery,
+);
+
+const PokemonList = () => {
+  const [result] = useQuery({ query: PersistedQuery });
+
+  const { data, fetching, error } = result;
+
+  if (error) {
+    return (
+      <>
+        <h3>Oh no!</h3>
+        <pre>{error.message}</pre>
+      </>
+    );
+  } else if (fetching || !data) {
+    return <h3>Loading...</h3>
+  }
+
+  return (
+    <div>
+      {data.pokemons ? (
+        <ul>
+          {data.pokemons.map((pokemon, index) => (
+            <PokemonItem data={pokemon} key={pokemon?.id || index} />
+          ))}
+        </ul>
+      ) : (
+        <h3>Your Pokedex is empty.</h3>
+      )}
+    </div>
+  );
+};
+
+export { PokemonList };
